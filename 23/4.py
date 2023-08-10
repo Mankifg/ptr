a6cb672f0520a89e4868819da58d67ccf1e17d4d
Mankifg <83959255+Mankifg@users.noreply.github.com>
2023-08-10 13:31:04 +0200

    last change (prob late)

--- 23/4.py
+++ 23/4.py
@@ -16,94 +16,39 @@ Primer
 Naloga
 Izračunaj, koliko sidrnih mest mora prečkati reševalna ladja, ki hiti na pomoč ladji Umag 2."""
 
-# https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
+from collections import deque
 
+def is_valid_move(grid, row, col):
+    rows, cols = len(grid), len(grid[0])
+    return 0 <= row < rows and 0 <= col < cols and grid[row][col] == 0
 
-class Node:
-    """A node class for A* Pathfinding"""
+def shortest_path(grid, start, end):
+    rows, cols = len(grid), len(grid[0])
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    
+    visited = set()
+    queue = deque([(start, 0)])  # Queue elements: ((row, col), distance)
+    visited.add(start)
+    
+    while queue:
+        (row, col), distance = queue.popleft()
+        
+        if (row, col) == end:
+            return distance
+        
+        for dr, dc in directions:
+            new_row, new_col = row + dr, col + dc
+            if is_valid_move(grid, new_row, new_col) and (new_row, new_col) not in visited:
+                queue.append(((new_row, new_col), distance + 1))
+                visited.add((new_row, new_col))
+    
+    return -1
 
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
-
-        self.g = 0
-        self.h = 0
-        self.f = 0
-
-    def __eq__(self, other):
-        return self.position == other.position
-
-
-def astar(maze, start, end):
-    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
-
-    start_node = Node(None, start)
-    start_node.g = start_node.h = start_node.f = 0
-    end_node = Node(None, end)
-    end_node.g = end_node.h = end_node.f = 0
-
-    open_list = []
-    closed_list = []
-
-    open_list.append(start_node)
-
-    while len(open_list) > 0:
-        current_node = open_list[0]
-        current_index = 0
-        for index, item in enumerate(open_list):
-            if item.f < current_node.f:
-                current_node = item
-                current_index = index
-        open_list.pop(current_index)
-        closed_list.append(current_node)
-        if current_node == end_node:
-            path = []
-            current = current_node
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
-            return path[::-1]
-        children = []
-        # for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]:  # Adjacent squares
-            node_position = (
-                current_node.position[0] + new_position[0],
-                current_node.position[1] + new_position[1],
-            )
-            if (
-                node_position[0] > (len(maze) - 1)
-                or node_position[0] < 0
-                or node_position[1] > (len(maze[len(maze) - 1]) - 1)
-                or node_position[1] < 0
-            ):
-                continue
-
-            if maze[node_position[1]][node_position[0]] != 0:
-                continue
-
-            new_node = Node(current_node, node_position)
-
-            children.append(new_node)
-
-        for child in children:
-            for closed_child in closed_list:
-                if child == closed_child:
-                    continue
-            child.g = current_node.g + 1
-            child.h = ((child.position[0] - end_node.position[0]) ** 2) + (
-                (child.position[1] - end_node.position[1]) ** 2
-            )
-            child.f = child.g + child.h
-            for open_node in open_list:
-                if child == open_node and child.g > open_node.g:
-                    continue
-            open_list.append(child)
 
 # my code
  
 def change(x, y):
     b[y][x] = 1
-
 
 import numpy as np
 
@@ -136,18 +81,9 @@ for _ in range(n):
         for x in range(x1, x2 + 1):
             b[key_y][x] = 1
 
-path = astar(b, start, end)
+path = shortest_path(b, start, end)
 
-for i in range(len(path)):
-    b[path[i][1]][path[i][0]] = 9
-
-b[start[1]][start[0]] = 3
-b[end[1]][end[0]] = 3
-
-print(len(path) - 1)
-
-# print(path)
-# print(b)
+print(path)     
 
 """
 10 10
